77d7e9b8ebbc649b4695f37acb51ee9ce194a3eb
aaa-arya <aryasujeet680@gmail.com>
2023-12-12 00:00:11 +0530

    mes

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+const app=express();
+const port=9000;
+// const serverless = require('serverless-http');
+
+
+app.use("/",(req,res)=>{
+    res.json({message :"helooo from api"});
+});
+app.listen(9000,()=>{
+
+
+    console.log('starting server on port 9000');
+})
+
+// let record=[];
+
+
+// // app.listen(
+// //     PORT,
+// //     ()=> console.log('server running ${PORT}')
+// // );
+// router.get('/t',(req,res) =>{
+//     res.send('200 sujjjet');
+
+// });
+
+// router.get('/',(req,res) =>{
+//     res.send('200 sujjjet');
+
+// });
+
+
+
