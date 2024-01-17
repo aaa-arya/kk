613a0020f11ee58b71c475b5d73cb6580bfa0825
aaa-arya <aryasujeet680@gmail.com>
2024-01-17 12:36:55 +0530

    ds

--- index.js
+++ index.js
@@ -15,12 +15,16 @@ app.use("/",(req,res)=>{
 
 
 
-    fetch("https://jsonplaceholder.typicode.com/todos", {
+    fetch("https://nearbypro.www-2048.com/mates/en/convert?id=vNyx2OEEqHk", {
         method: "POST",
         body: JSON.stringify({
-          userId: 1,
-          title: "Fix my bugs",
-          completed: false
+            platform:'youtube',
+                    url:'https://www.youtube.com/watch?v=vNyx2OEEqHk',
+                    title:'title33',
+                    id:'vNyx2OEEqHk',
+                    ext:'mp3',
+                    note:'128k',
+                    format:'128k'
         }),
         headers: {
           "Content-type": "application/json; charset=UTF-8"
