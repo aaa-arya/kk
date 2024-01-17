7a1efc3e359c9ed3744c03210ddb517112557447
aaa-arya <aryasujeet680@gmail.com>
2024-01-17 11:22:49 +0530

    chane

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 import express from 'express';
 const app=express();
 const port=9000;
-// const serverless = require('serverless-http');
 
+app.use("/i",(req,res)=>{
+    res.render("index");
+
+
+});
+
+
+app.set("view engine","ejs");
 
 app.use("/",(req,res)=>{
     res.json({message :"helooo from api"});
+
+
 });
 app.listen(9000,()=>{
 
 
     console.log('startingc server ii on port 9000');
+
+
+    
 })
 
-// let record=[];
-
-
-// // app.listen(
-// //     PORT,
-// //     ()=> console.log('server running ${PORT}')
-// // );
-// router.get('/t',(req,res) =>{
-//     res.send('200 sujjjet');
-
-// });
-
-// router.get('/',(req,res) =>{
-//     res.send('200 sujjjet');
-
-// });
 
 
 
