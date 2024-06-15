2019fe927b38a0ab4b26dc2f797dedfa62112ece
aaa-arya <aryasujeet680@gmail.com>
2024-06-15 16:35:55 +0530

    xc

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set("view engine","ejs");
 
 app.use('/video',(req,res)=>{
 
-  res.json("okkkk done");
+
 
   const videoUrl='https://instagram.ftip5-1.fna.fbcdn.net/o1/v/t16/f1/m82/B44D135EE0CB25E3A4A02EDE90C86BA8_video_dashinit.mp4?efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0&_nc_ht=instagram.ftip5-1.fna.fbcdn.net&_nc_cat=111&vs=6549837695039591_3329541574&_nc_vs=HBksFQIYT2lnX3hwdl9yZWVsc19wZXJtYW5lbnRfcHJvZC9CNDREMTM1RUUwQ0IyNUUzQTRBMDJFREU5MEM4NkJBOF92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dBSDV4eGZDUWQwZkxZMERBQk1tMUwwQnZGczlicV9FQUFBRhUCAsgBACgAGAAbABUAACaS4LzmjITqPxUCKAJDMywXQDhu2RaHKwIYEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%3D%3D&_nc_rid=259858606a&ccb=9-4&oh=00_AYBUt6LjnkCtf-owE1Oi11iULE1wKDI273ObBVtxf0qmXw&oe=666EF225&_nc_sid=2999b8&dl=1';
  
@@ -28,7 +28,7 @@ app.use('/video',(req,res)=>{
   const path = Path.resolve(__dirname, 'media', 'video.mp4');//D:\material\VS CODE\WEBREQUEST\iii\media\video.mp4
   
   ffmpeg.setFfmpegPath(ffmpegPath);
-
+  res.json("okkkk done");
 
 //   ffmpeg(videoUrl)
 // .videoCodec('libx264')
