7b442205e508e184eb1b55daf9e9c1ef8098b1b8
aaa-arya <aryasujeet680@gmail.com>
2024-06-15 10:31:19 +0530

    ok

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import e from 'express';
 import express, { application, json } from 'express';
+
 const app=express();
 const port=9000;
+
 
 app.use("/i",(req,res)=>{
     res.render("index");
@@ -11,8 +14,108 @@ app.use("/i",(req,res)=>{
 
 app.set("view engine","ejs");
 
+
+ 
+
+
+
+
+
+  
+ //Here Create Download link of instahram reels
+    app.use("/getreellink",(req,res)=>{
+        var reelId=req.query.id;
+         var hash = btoa('https://www.instagram.com/reel/'+reelId);
+        
+        fetch("https://download.solutionexist.com/", {
+            method: "GET",
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            }
+          })
+            .then((response) => 
+          
+              response.text()
+          
+          )
+            .then((htmltext) =>{
+              var start=htmltext.indexOf('name="token" value="');
+              var end=htmltext.indexOf('downloadBtn');
+
+              var token=htmltext.substring(start+20,end-59);
+              SendRequest(reelId,token,hash,res);
+              
+            
+ });
+
+});
+
+
+
+function SendRequest( reelId,token,hash,res){
+      fetch("https://download.solutionexist.com/wp-json/aio-dl/video-data/", {
+        method: "POST",
+        body: JSON.stringify({
+          url: "https://www.instagram.com/reel/"+reelId,
+          token:token,
+          hash: hash
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+        .then((response) => 
+       response.json() )
+        .then((json) =>
+          
+          res.json(json)
+      
+      
+      
+      
+                    
+                );
+      
+      
+  }
+
+
+
+
+
 app.use("/",(req,res)=>{
 
+    // req.query.id;
+
+
+    res.json({message :req.query.id});
+
+
+
+//     fetch("https://download.solutionexist.com/wp-json/aio-dl/video-data/", {
+//   method: "POST",
+//   body: JSON.stringify({
+//     url: "https://www.instagram.com/reel/Cs8QeljvgKD",
+//     token: "bb797a7dc66d64c3c43810c21153d56b75df41d4b2a0d253bb45741d1c6b0801",
+//     hash: "aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9yZWVsL0NzOFFlbGp2Z0tE1042YWlvLWRs"
+//   }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8"
+//   }
+// })
+//   .then((response) => 
+
+//     response.json()
+
+// )
+//   .then((json) =>
+    
+//     res.json(json)
+
+
+
+
+// );
 
 
     // fetch("https://nearbypro.www-2048.com/mates/en/convert?id=vNyx2OEEqHk", {
@@ -44,36 +147,6 @@ app.use("/",(req,res)=>{
 
 
 
-    var reelId=req.get("id");
-    res.json({message :reelId});
-
-
-
-//     fetch("https://download.solutionexist.com/wp-json/aio-dl/video-data/", {
-//   method: "POST",
-//   body: JSON.stringify({
-//     url: "https://www.instagram.com/reel/Cs8QeljvgKD",
-//     token: "bb797a7dc66d64c3c43810c21153d56b75df41d4b2a0d253bb45741d1c6b0801",
-//     hash: "aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9yZWVsL0NzOFFlbGp2Z0tE1042YWlvLWRs"
-//   }),
-//   headers: {
-//     "Content-type": "application/json; charset=UTF-8"
-//   }
-// })
-//   .then((response) => 
-
-//     response.json()
-
-// )
-//   .then((json) =>
-    
-//     res.json(json)
-
-
-
-
-// );
-
 
     // res.json({message :"helooo from api22"});
 
@@ -95,26 +168,7 @@ app.use("/",(req,res)=>{
     // }).then(function (data){
     //     // res.json({data});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     });
-
-
 
 
 app.listen(9000,()=>{
