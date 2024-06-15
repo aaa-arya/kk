4efa8675b659a1a6193775dde67cd9d3a2edb290
aaa-arya <aryasujeet680@gmail.com>
2024-06-15 12:54:05 +0530

    fd

---
+++ filecompress.js
@@ -0,0 +1,17 @@
+// const fsysytem=fs();
+
+function print2(){
+
+    console.log("done");
+}
+
+// app.use('/video',(req,res)=>{
+
+//     // const videoUrl="https://instagram.ftip5-1.fna.fbcdn.net/o1/v/t16/f1/m82/B44D135EE0CB25E3A4A02EDE90C86BA8_video_dashinit.mp4?efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0&_nc_ht=instagram.ftip5-1.fna.fbcdn.net&_nc_cat=111&vs=6549837695039591_3329541574&_nc_vs=HBksFQIYT2lnX3hwdl9yZWVsc19wZXJtYW5lbnRfcHJvZC9CNDREMTM1RUUwQ0IyNUUzQTRBMDJFREU5MEM4NkJBOF92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dBSDV4eGZDUWQwZkxZMERBQk1tMUwwQnZGczlicV9FQUFBRhUCAsgBACgAGAAbABUAACaS4LzmjITqPxUCKAJDMywXQDhu2RaHKwIYEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%3D%3D&_nc_rid=259858606a&ccb=9-4&oh=00_AYBUt6LjnkCtf-owE1Oi11iULE1wKDI273ObBVtxf0qmXw&oe=666EF225&_nc_sid=2999b8&dl=1";
+  
+//     // const videSTream=fs.createReadStream(videoUrl);
+  
+//     // console.log(fs.statSync(videoUrl).size);
+//     // videSTream.pipe(res);
+  
+//   })
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-import e from 'express';
-import express, { application, json } from 'express';
+// import e from 'express';
+// import express, { application, json } from 'express';
 
+
+// var dd=require("./filecompress.js");
+
+// import f from 'fs';
+// import fs,{applicationj,json1} from 'fs';
+
+const express=require('express');
 const app=express();
+
+// const app=express();
 const port=9000;
 
 
 app.use("/i",(req,res)=>{
     res.render("index");
-
-    
-
-
 });
 
 
@@ -48,6 +53,8 @@ app.set("view engine","ejs");
 
 
 
+
+
 function SendRequest( reelId,token,hash,res){
       fetch("https://download.solutionexist.com/wp-json/aio-dl/video-data/", {
         method: "POST",
@@ -72,6 +79,18 @@ function SendRequest( reelId,token,hash,res){
       
   }
 
+  app.use('/video',(req,res)=>{
+
+    dd.print2();
+
+    // const videoUrl="https://instagram.ftip5-1.fna.fbcdn.net/o1/v/t16/f1/m82/B44D135EE0CB25E3A4A02EDE90C86BA8_video_dashinit.mp4?efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0&_nc_ht=instagram.ftip5-1.fna.fbcdn.net&_nc_cat=111&vs=6549837695039591_3329541574&_nc_vs=HBksFQIYT2lnX3hwdl9yZWVsc19wZXJtYW5lbnRfcHJvZC9CNDREMTM1RUUwQ0IyNUUzQTRBMDJFREU5MEM4NkJBOF92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dBSDV4eGZDUWQwZkxZMERBQk1tMUwwQnZGczlicV9FQUFBRhUCAsgBACgAGAAbABUAACaS4LzmjITqPxUCKAJDMywXQDhu2RaHKwIYEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%3D%3D&_nc_rid=259858606a&ccb=9-4&oh=00_AYBUt6LjnkCtf-owE1Oi11iULE1wKDI273ObBVtxf0qmXw&oe=666EF225&_nc_sid=2999b8&dl=1";
+  
+    // const videSTream=fs.createReadStream(videoUrl);
+  
+    // console.log(fs.statSync(videoUrl).size);
+    // videSTream.pipe(res);
+  
+  })
 
 
 app.use("/",(req,res)=>{
